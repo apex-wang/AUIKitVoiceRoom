0b8ac541224c2faeb6b10fd23c9c33b31f287202
apex-wang <1746807718@qq.com>
2023-08-31 16:34:41 +0800

    fix bug unbindRespDelegate

--- Android/asceneskit/src/main/java/io/agora/asceneskit/voice/binder/AUIGiftBarrageBinder.kt
+++ Android/asceneskit/src/main/java/io/agora/asceneskit/voice/binder/AUIGiftBarrageBinder.kt
@@ -42,7 +42,7 @@ class AUIGiftBarrageBinder constructor(
     }
 
     override fun unBind() {
-        giftService.bindRespDelegate(null)
+        giftService.unbindRespDelegate(this)
     }
 
     fun showBottomGiftDialog(){
